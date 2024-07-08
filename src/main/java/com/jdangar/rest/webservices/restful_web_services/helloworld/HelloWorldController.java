37f4d55d41ee508004683ff30f0053b8dd6c66e4
JDangar <dangarjignesh9@gmail.com>
2024-07-08 22:03:39 +0530

    Hello World String and Bean GetMapping API

---
+++ src/main/java/com/jdangar/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.jdangar.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorld helloWorldBean(){
+        return new HelloWorld("Hello World");
+    }
+}
