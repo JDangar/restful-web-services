9d6554ae01d08d18356cd3325e503e8948ebe741
JDangar <dangarjignesh9@gmail.com>
2024-07-10 06:35:11 +0530

    REST API validation and customized message

--- src/main/java/com/jdangar/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/jdangar/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.jdangar.rest.webservices.restful_web_services.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -33,7 +34,7 @@ public class UserResource {
     }
 
     @PostMapping(path = "/users")
-    public ResponseEntity<Object> saveUser(@RequestBody User user) {
+    public ResponseEntity<Object> saveUser(@Valid @RequestBody User user) {
         User savedUser = userDaoService.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
