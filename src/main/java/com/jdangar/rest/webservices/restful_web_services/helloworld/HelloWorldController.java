4cc258ca10adbce44850503251b5cf0e5cef21f7
JDangar <dangarjignesh9@gmail.com>
2024-07-12 06:30:02 +0530

    Explored internationalization for REST API

--- src/main/java/com/jdangar/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/jdangar/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.jdangar.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +11,32 @@ import java.util.Locale;
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping(path = "/hello-world-bean")
-    public HelloWorld helloWorldBean(){
+    public HelloWorld helloWorldBean() {
         return new HelloWorld("Hello World");
     }
 
     @GetMapping(path = "/hello-world/{name}")
-    public HelloWorld helloWorldPathVariable(@PathVariable String name){
+    public HelloWorld helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorld(String.format(Locale.US, "Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalization")
+    public String helloWorldInternationalization() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
