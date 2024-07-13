7483b2833290f857b8ba4db1259abc803d879ce7
JDangar <dangarjignesh9@gmail.com>
2024-07-14 02:01:56 +0530

    Static filtering

---
+++ src/main/java/com/jdangar/rest/webservices/restful_web_services/filtering/FilteringController.java
@@ -0,0 +1,14 @@
+package com.jdangar.rest.webservices.restful_web_services.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean filtering(){
+        return new SomeBean("value1","value2","value3");
+    }
+
+}
